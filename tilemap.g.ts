e0ec3d9e6de753aad566a8eb077d70c73fd9cda7
C3inator <71284268+C3inator@users.noreply.github.com>
2020-10-28 13:28:32 -0400

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,61 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+
+    helpers.registerTilemapFactory(function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level": return tiles.createTilemap(hex`1000100001010101010303030303030303030101010101010100000000000000000001010101010101000000000000000000010101010101010000000000000000000000010101010100000000000000000002000101010101000000000000010101010101010101010000000000000101010101010101010100000000000001010101010300000000000000000000010101010103000000000000000000000101010101030000000001010100000001010101010300000000010101000000010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101`, img`
+2 2 2 2 2 . . . . . . . . . 2 2 
+2 2 2 2 2 . . . . . . . . . 2 2 
+2 2 2 2 2 . . . . . . . . . 2 2 
+2 2 2 2 2 . . . . . . . . . . . 
+2 2 2 2 2 . . . . . . . . . . . 
+2 2 2 2 2 . . . . . . 2 2 2 2 2 
+2 2 2 2 2 . . . . . . 2 2 2 2 2 
+2 2 2 2 2 . . . . . . 2 2 2 2 2 
+. . . . . . . . . . . 2 2 2 2 2 
+. . . . . . . . . . . 2 2 2 2 2 
+. . . . . 2 2 2 . . . 2 2 2 2 2 
+. . . . . 2 2 2 . . . 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile3,myTiles.tile6], TileScale.Sixteen)
+            case "level_0": return tiles.createTilemap(hex`1000100001010101010303030303030303030101010101010100000000000000000001010101010101000000000000000000010101010101010000000000000000000000010101010100000000000000000002000101010101000000000000010101010101010101010000000000000101010101010101010100000000000001010101010300000000000000000000010101010103000000000000000000000101010101030000000001010100000001010101010300000000010101000000010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101`, img`
+2 2 2 2 2 . . . . . . . . . 2 2 
+2 2 2 2 2 . . . . . . . . . 2 2 
+2 2 2 2 2 . . . . . . . . . 2 2 
+2 2 2 2 2 . . . . . . . . . . . 
+2 2 2 2 2 . . . . . . . . . . . 
+2 2 2 2 2 . . . . . . 2 2 2 2 2 
+2 2 2 2 2 . . . . . . 2 2 2 2 2 
+2 2 2 2 2 . . . . . . 2 2 2 2 2 
+. . . . . . . . . . . 2 2 2 2 2 
+. . . . . . . . . . . 2 2 2 2 2 
+. . . . . 2 2 2 . . . 2 2 2 2 2 
+. . . . . 2 2 2 . . . 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile3,myTiles.tile6], TileScale.Sixteen)
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
